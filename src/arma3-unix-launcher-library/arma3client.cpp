6f732d5508d7226be9e6c6308f217ac667d3674b
Muttley <muttdini@gmail.com>
2019-03-16 11:54:46 +0100

    Add missing dependency + move TODO

--- src/arma3-unix-launcher-library/arma3client.cpp
+++ src/arma3-unix-launcher-library/arma3client.cpp
@@ -163,7 +163,7 @@ namespace ARMA3
 
     std::string Client::PickModName(Mod const &mod, std::vector<std::string> const &names)
     {
-        TODO_BEFORE(03, 2019, "Pick mod name from workshop");
+        TODO_BEFORE(04, 2019, "Pick mod name from workshop");
         for (auto const &name : names)
         {
             if (ContainsKey(mod.KeyValue, name))
