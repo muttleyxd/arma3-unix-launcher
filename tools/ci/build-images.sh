0f15dda87b8e22908c41a0c78c983b7fe7136410
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-03-23 00:32:28 +0100

    build-images.sh: add space awareness

--- tools/ci/build-images.sh
+++ tools/ci/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SELF_PATH=`dirname "$(readlink -f $0)"`
-pushd $SELF_PATH
+pushd "$SELF_PATH" >/dev/null
 
 #no cache because mirrors get invalidated quickly
 docker build --no-cache -t a3ul_archlinux_build -f docker/Dockerfile.a3ul_archlinux_build .
@@ -9,4 +9,4 @@ docker build --no-cache -t a3ul_ubuntu-18.04_build -f docker/Dockerfile.a3ul_ubu
 
 docker build --no-cache -t github-release -f docker/Dockerfile.github-release .
 
-popd
+popd >/dev/null
