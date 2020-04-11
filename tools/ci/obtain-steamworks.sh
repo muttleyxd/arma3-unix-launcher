c8bbd011eb5409d639c3fb19c78cd8f7276c119a
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-04-11 16:31:04 +0200

    ci: fix deployment

--- tools/ci/obtain-steamworks.sh
+++ tools/ci/obtain-steamworks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ -z $GPG_DECRYPTION_KEY_BASE64 ]; then
-    echo "This script is intended for CI usage by arma3-unix-launcher, you shouldn't run it"
+if [ -f /tmp/key.gpg ]; then
+    echo "This script is intended for CI usage by arma3-unix-launcher, you shouldn't run it manually"
     exit 1
 fi
 
@@ -8,7 +8,6 @@ set -euo pipefail
 
 mkdir /tmp/steamworks && cd /tmp/steamworks >/dev/null 2>&1
 wget https://github.com/muttleyxd/gpg_encrypted_secrets/releases/download/secrets/steamworks.7z.gpg >/dev/null 2>&1
-echo $GPG_DECRYPTION_KEY_BASE64 | base64 -d >/tmp/key.gpg >/dev/null 2>&1
 gpg --import /tmp/key.gpg >/dev/null 2>&1
 gpg --output steamworks.7z --decrypt steamworks.7z.gpg >/dev/null 2>&1
 7z x steamworks.7z >/dev/null 2>&1
