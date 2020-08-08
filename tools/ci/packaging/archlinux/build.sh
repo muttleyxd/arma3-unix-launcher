babcd6ba38298798cd5bc9f113a534ab96606cab
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-08-08 13:25:35 +0200

    tools: ci: packaging: archlinux: use local build directory instead of cloning

--- tools/ci/packaging/archlinux/build.sh
+++ tools/ci/packaging/archlinux/build.sh
@@ -14,5 +14,9 @@ cp ./PKGBUILD $BUILD_DIR/PKGBUILD
 cd $BUILD_DIR
 sed -i "s|/A3ULPATH|$A3UL_DIR|g" PKGBUILD
 sed -i "s|/tmp/steamworks|$STEAMWORKS_SDK_PATH|g" PKGBUILD
+
+mkdir src
+cp -RL /$A3UL_DIR/* ./src
+rm -rf ./src/build*
 makepkg
-cp -n /tmp/build/*.tar.xz $OUTPUT_DIR
+cp -n /tmp/build/*.tar.* $OUTPUT_DIR
