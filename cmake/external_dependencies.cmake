fb005d360c791f5a23b729eacbd242b2bec4db35
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-08-17 18:32:54 +0200

    cmake: switch to fmt-header-only
    fixes issues when library mismatch could appear

--- cmake/external_dependencies.cmake
+++ cmake/external_dependencies.cmake
@@ -78,12 +78,7 @@ function(setup_fmt)
     setup_library("${CHECK_SOURCE}"
                   NAME fmt
                   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-                  TEST_LINK_LIBS fmt
                   )
-    get_target_property(TARGET_TYPE fmt::fmt TYPE)
-    if ("${TARGET_TYPE}" STREQUAL "SHARED_LIBRARY")
-        target_compile_options(fmt::fmt INTERFACE "-fpic")
-    endif()
 endfunction()
 
 function(setup_nlohmann_json)
