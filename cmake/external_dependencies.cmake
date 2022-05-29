13cde1e309ddb6bfa3076dc772e63da2041a1a46
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2022-05-29 20:38:12 +0200

    cmake: ignore redundant move warning for spdlog

--- cmake/external_dependencies.cmake
+++ cmake/external_dependencies.cmake
@@ -191,7 +191,7 @@ function(setup_spdlog)
                   GIT_TAG v1.x
                   TEST_DEFINITIONS -DSPDLOG_FMT_EXTERNAL
                   TEST_LINK_LIBS ${FMT_TARGET_NAME}
-                  CXX_FLAGS "-Wno-attributes -Wno-reorder"
+                  CXX_FLAGS "-Wno-attributes -Wno-reorder -Wno-redundant-move"
                   )
 endfunction()
 
