c5713557a297db2197c58f3bf373e107180bd796
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2022-09-09 22:18:51 +0200

    cmake: external_dependencies: lock fmt version to 8.1.1
    fixes build on Ubuntu 16.04 and 18.04

--- cmake/external_dependencies.cmake
+++ cmake/external_dependencies.cmake
@@ -101,6 +101,7 @@ function(setup_fmt)
     setup_library("${CHECK_SOURCE}"
                   NAME fmt
                   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+                  GIT_TAG 8.1.1
                   TEST_LINK_LIBS fmt
                   )
 
